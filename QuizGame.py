dfbaa3b4c291fb08985f21c20b9b83ceb950d54b
Rakshith-Sudan <raksud15oct@gmail.com>
2022-06-21 10:20:53 +0530

    f

--- QuizGame.py
+++ QuizGame.py
@@ -7,9 +7,12 @@ if playing.lower() != "yes":
 
 print("Okay let's play :)")
 
+score = 0
+
 answer = input("What does CPU stand for? ")
 if answer.lower() == "central processing unit":
     print("Correct!")
+    score += 1
 else:
     print("Incorrect!")
 
@@ -17,6 +20,7 @@ else:
 answer = input("What does GPU stand for? ")
 if answer.lower() == "graphics processing unit":
     print("Correct!")
+    score += 1
 else:
     print("Incorrect!")
 
@@ -24,6 +28,7 @@ else:
 answer = input("What does RAM stand for? ")
 if answer.lower() == "random access memory":
     print("Correct!")
+    score += 1
 else:
     print("Incorrect!")
 
@@ -31,5 +36,9 @@ else:
 answer = input("What does PSU stand for? ")
 if answer.lower() == "power supply":
     print("Correct!")
+    score += 1
 else:
     print("Incorrect!")
+
+print("You Got " + str(score) + " questions correct!")
+print("You Got " + str((score/4)*100) + " %.")
